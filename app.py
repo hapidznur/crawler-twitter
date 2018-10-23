560d98ebc0b1a335be51b544be82e3ecd3a2e8fa
hapidznur <hapidznur@gmail.com>
2018-10-23 21:36:20 +0700

    enviroment example

---
+++ app.py
@@ -0,0 +1,10 @@
+import streaming
+
+class Crawler:
+
+    def get_timeline(self, user):     
+        return streaming.user_timeline(user)
+
+
+crawler = Crawler()
+print(crawler.get_timeline('elfarqy'))
--- streaming.py
+++ streaming.py
@@ -1,112 +1,124 @@
-import twitter
 from decouple import config
+from datetime import datetime
+import twitter
 
 consumer_key = config('CONSUMER_KEY')
 consumer_secret = config('CONSUMER_SECRET')
 access_token = config('ACCESS_TOKEN')
-acesss_secret = config('ACCESS_SECRET')
+access_secret = config('ACCESS_SECRET')
 
-twitter = Twitter(auth = OAuth(access_token,access_secret,consumer_key,consumer_secret))
-
-# source from https://github.com/ideoforms/
+api = twitter.Api(consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token_key=access_token,
+    access_token_secret=access_secret,
+    tweet_mode= 'extended',
+    sleep_on_rate_limit=True)
 
 # User Timeline
 user = "ideoforms"
+# print(api.VerifyCredentials())
+# statuses = api.GetUserTimeline(screen_name='elfarqy')
+# for f in statuses:
+#     print(f.full_text)
 
-#-----------------------------------------------------------------------
-# query the user timeline.
-# twitter API docs:
-# https://dev.twitter.com/rest/reference/get/statuses/user_timeline
-#-----------------------------------------------------------------------
-results = twitter.statuses.user_timeline(screen_name = user)
+results = api.GetSearch(raw_query='q=%232019GANTIPRESIDEN%20&result_type=recent&since=2018-07-19&count=100')
+# api.GetUserTimeline(user)
+# def user_timeline(username):
+#     #-----------------------------------------------------------------------
+#     # query the user timeline.
+#     # twitter API docs:
+#     # https://dev.twitter.com/rest/reference/get/statuses/user_timeline
+#     #-----------------------------------------------------------------------
+#     results = twitter.statuses.user_timeline(screen_name = username)
+#     print(results[1])
+#     #-----------------------------------------------------------------------
+#     # loop through each status item, and print its content.
+#     #-----------------------------------------------------------------------
+# for status in statuses:
+    # print("(%s) %s" % (status["created_at"], status["text"].encode("ascii", "ignore")))
 
-#-----------------------------------------------------------------------
-# loop through each status item, and print its content.
-#-----------------------------------------------------------------------
-for status in results:
-    print("(%s) %s" % (status["created_at"], status["text"].encode("ascii", "ignore")))
+# def trends_by_place(id):
+#     #-----------------------------------------------------------------------
+#     # retrieve global trends.
+#     # other localised trends can be specified by looking up WOE IDs:
+#     #   http://developer.yahoo.com/geo/geoplanet/
+#     # twitter API docs: https://dev.twitter.com/rest/reference/get/trends/place
+#     #-----------------------------------------------------------------------
+#     results = twitter.trends.place(_id = 23424975)
 
-#-----------------------------------------------------------------------
-# retrieve global trends.
-# other localised trends can be specified by looking up WOE IDs:
-#   http://developer.yahoo.com/geo/geoplanet/
-# twitter API docs: https://dev.twitter.com/rest/reference/get/trends/place
-#-----------------------------------------------------------------------
-results = twitter.trends.place(_id = 23424975)
+#     print("UK Trends")
 
-print("UK Trends")
+#     for location in results:
+#         for trend in location["trends"]:
+#             print(" - %s" % trend["name"])
 
-for location in results:
-    for trend in location["trends"]:
-        print(" - %s" % trend["name"])
+# def search_by_user(username, text):
+#     #-----------------------------------------------------------------------
+#     # perform a basic search 
+#     # twitter API docs: https://dev.twitter.com/rest/reference/get/statuses/user_timeline
+#     #-----------------------------------------------------------------------
+#     results = twitter.statuses.user_timeline(screen_name = user)
 
-#-----------------------------------------------------------------------
-# perform a basic search 
-# twitter API docs: https://dev.twitter.com/rest/reference/get/statuses/user_timeline
-#-----------------------------------------------------------------------
-results = twitter.statuses.user_timeline(screen_name = user)
+#     #-----------------------------------------------------------------------
+#     # loop through each of my statuses, and print its content
+#     #-----------------------------------------------------------------------
+#     print(twitter)
+#     # for status in results:
+#         # print("@%s %s" % (user, status["full_text"]))
 
-#-----------------------------------------------------------------------
-# loop through each of my statuses, and print its content
-#-----------------------------------------------------------------------
-for status in results:
-    print("@%s %s" % (user, status["text"]))
+#     #-----------------------------------------------------------------------
+#     # do a new query: who has RT'd this tweet?
+#     #-----------------------------------------------------------------------
+#     # retweets = twitter.statuses.retweets._id(_id = status["id"])
+#     # for retweet in retweets:
+#     #     print(" - retweeted by %s" % (retweet["user"]["screen_name"]))
 
-            #-----------------------------------------------------------------------
-# do a new query: who has RT'd this tweet?
-                    #-----------------------------------------------------------------------
-retweets = twitter.statuses.retweets._id(_id = status["id"])
-for retweet in retweets:
-    print(" - retweeted by %s" % (retweet["user"]["screen_name"]))
+# def home():
+#     #-----------------------------------------------------------------------
+#     # request my home timeline
+#     # twitter API docs: https://dev.twitter.com/rest/reference/get/statuses/home_timeline
+#     #-----------------------------------------------------------------------
+#     statuses = twitter.statuses.home_timeline(count = 50)
+#     print(statuses)
+#     #-----------------------------------------------------------------------
+#     # loop through each of my statuses, and print its content
+#     #-----------------------------------------------------------------------
+#     for status in statuses:
+#         print("(%s) @%s %s" % (status["created_at"], status["user"]["screen_name"], status["text"]))
 
+# def search(text):
+#     #-----------------------------------------------------------------------
+#     # perform a user search 
+#     # twitter API docs: https://dev.twitter.com/rest/reference/get/users/search
+#     #-----------------------------------------------------------------------
+#     results = twitter.users.search(q = '"New Cross"')
+#     #-----------------------------------------------------------------------
+#     # loop through each of the users, and print their details
+#     #-----------------------------------------------------------------------
+#     for user in results:
+#         print("@%s (%s): %s" % (user["screen_name"], user["name"], user["location"]))
+# def tweet_search(text):
+#     #-----------------------------------------------------------------------
+#     # perform a basic search 
+#     # twitter API docs: https://dev.twitter.com/rest/reference/get/search/tweets
+#     #-----------------------------------------------------------------------
+#     terms = "pink elephants"
+#     created_at_format = '%a %b %d %H:%M:%S +0000 %Y'
+#     query = twitter.search.tweets(q = terms)
+#     results = query["statuses"]
+#     #-----------------------------------------------------------------------
+#     # take the timestamp of the first and last tweets in these results,
+#     # and calculate the average time between tweets.
+#     #-----------------------------------------------------------------------
+#     first_timestamp = datetime.strptime(results[0]["created_at"], created_at_format)
+#     last_timestamp = datetime.strptime(results[-1]["created_at"], created_at_format)
+#     total_dt = (first_timestamp - last_timestamp).total_seconds()
+#     mean_dt = total_dt / len(results)
 
-#-----------------------------------------------------------------------
-# request my home timeline
-# twitter API docs: https://dev.twitter.com/rest/reference/get/statuses/home_timeline
-#-----------------------------------------------------------------------
-statuses = twitter.statuses.home_timeline(count = 50)
-print(statuses)
+#     #-----------------------------------------------------------------------
+#     # print the average of the differences
+#     #-----------------------------------------------------------------------
+#     print("Average tweeting rate for '%s' between %s and %s: %.3fs" % (terms,
+#     results[-1]["created_at"], results[ 0]["created_at"], mean_dt))
 
-#-----------------------------------------------------------------------
-# loop through each of my statuses, and print its content
-#-----------------------------------------------------------------------
-for status in statuses:
-    print("(%s) @%s %s" % (status["created_at"], status["user"]["screen_name"], status["text"]))
-
-
-#-----------------------------------------------------------------------
-# perform a user search 
-# twitter API docs: https://dev.twitter.com/rest/reference/get/users/search
-#-----------------------------------------------------------------------
-results = twitter.users.search(q = '"New Cross"')
-
-#-----------------------------------------------------------------------
-# loop through each of the users, and print their details
-#-----------------------------------------------------------------------
-for user in results:
-    print("@%s (%s): %s" % (user["screen_name"], user["name"], user["location"]))
-
-
-
-#-----------------------------------------------------------------------
-# perform a basic search 
-# twitter API docs: https://dev.twitter.com/rest/reference/get/search/tweets
-#-----------------------------------------------------------------------
-terms = "pink elephants"
-query = twitter.search.tweets(q = terms)
-results = query["statuses"]
-
-                  #-----------------------------------------------------------------------
-                  # take the timestamp of the first and last tweets in these results,
-                  # and calculate the average time between tweets.
-                  #-----------------------------------------------------------------------
-first_timestamp = datetime.strptime(results[0]["created_at"], created_at_format)
-last_timestamp = datetime.strptime(results[-1]["created_at"], created_at_format)
-total_dt = (first_timestamp - last_timestamp).total_seconds()
-mean_dt = total_dt / len(results)
-
-                  #-----------------------------------------------------------------------
-                  # print the average of the differences
-                  #-----------------------------------------------------------------------
-print("Average tweeting rate for '%s' between %s and %s: %.3fs" % (terms,
-results[-1]["created_at"], results[ 0]["created_at"], mean_dt))
+# # source from https://github.com/ideoforms/
