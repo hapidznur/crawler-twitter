905ab897c481439cbbba12afe221b5cc56ef25c3
hapidznur <hapidznur@gmail.com>
2018-10-25 12:53:44 +0700

    Streaming

--- streaming.py
+++ streaming.py
@@ -1,6 +1,12 @@
 from decouple import config
 from datetime import datetime
+from model import mongo_add
 import twitter
+import logging
+import time
+import urllib
+from urllib.parse import quote
+import json
 
 consumer_key = config('CONSUMER_KEY')
 consumer_secret = config('CONSUMER_SECRET')
@@ -14,111 +20,69 @@ api = twitter.Api(consumer_key=consumer_key,
     tweet_mode= 'extended',
     sleep_on_rate_limit=True)
 
-# User Timeline
 user = "twitter"
-# print(api.VerifyCredentials())
-# statuses = api.GetUserTimeline(screen_name='elfarqy')
-# for f in statuses:
-#     print(f.full_text)
+def get_search(text):
+    raw_query='q=%s&result_type=recent&since=2018-08-10&count=1000' % (quote(text))
+    results = api.GetSearch(raw_query=raw_query)
+    for result in results:
+        data = {}
+        data['id_tweet'] = result.id_str
+        data['user_name'] = result.user.name
+        data['user_username'] = result.user.screen_name
+        data['user_id'] = result.user.id
+        # change twitter format to YYYY-MM-DD HH:MM:SS
+        data['created_at'] = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(result.created_at,'%a %b %d %H:%M:%S +0000 %Y'))
+        data['retweet_count'] = result.retweet_count
+        if result.retweeted_status:
+            data['retweeted_username'] = result.retweeted_status.user.screen_name
+            data['retweeted_id_tweet'] = result.retweeted_status.id_str
+            data['text'] = result.retweeted_status.full_text
+            data['tweet_created'] = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(result.retweeted_status.created_at,'%a %b %d %H:%M:%S +0000 %Y'))
 
-results = api.GetSearch(raw_query='q=%232019GANTIPRESIDEN%20&result_type=recent&since=2018-07-19&count=100')
-# api.GetUserTimeline(user)
-# def user_timeline(username):
-#     #-----------------------------------------------------------------------
-#     # query the user timeline.
-#     # twitter API docs:
-#     # https://dev.twitter.com/rest/reference/get/statuses/user_timeline
-#     #-----------------------------------------------------------------------
-#     results = twitter.statuses.user_timeline(screen_name = username)
-#     print(results[1])
-#     #-----------------------------------------------------------------------
-#     # loop through each status item, and print its content.
-#     #-----------------------------------------------------------------------
-# for status in statuses:
-    # print("(%s) %s" % (status["created_at"], status["text"].encode("ascii", "ignore")))
+        data['from'] = 'search'
+        mongo_add(data)
 
-# def trends_by_place(id):
-#     #-----------------------------------------------------------------------
-#     # retrieve global trends.
-#     # other localised trends can be specified by looking up WOE IDs:
-#     #   http://developer.yahoo.com/geo/geoplanet/
-#     # twitter API docs: https://dev.twitter.com/rest/reference/get/trends/place
-#     #-----------------------------------------------------------------------
-#     results = twitter.trends.place(_id = 23424975)
 
-#     print("UK Trends")
+def stream(text):
+    text = ['Pembakaran Bendara', 'Banser', 'MUI', 'Tauhid', 'Laskar Tauhid']    
+    with open('output.txt', 'a') as f:
+        # api.GetStreamFilter will return a generator that yields one status
+        # message (i.e., Tweet) at a time as a JSON dictionary.
+        for line in api.GetStreamFilter(track=text, filter_level='low'):
+            f.write(json.dumps(line))
+            f.write('\n')
 
-#     for location in results:
-#         for trend in location["trends"]:
-#             print(" - %s" % trend["name"])
+stream('test')
+# get_search('pembakaran bendera')
 
-# def search_by_user(username, text):
-#     #-----------------------------------------------------------------------
-#     # perform a basic search 
-#     # twitter API docs: https://dev.twitter.com/rest/reference/get/statuses/user_timeline
-#     #-----------------------------------------------------------------------
-#     results = twitter.statuses.user_timeline(screen_name = user)
+def tweet_url(t):
+    return "https://twitter.com/%s/status/%s" % (t.user.screen_name, t.id)
 
-#     #-----------------------------------------------------------------------
-#     # loop through each of my statuses, and print its content
-#     #-----------------------------------------------------------------------
-#     print(twitter)
-#     # for status in results:
-#         # print("@%s %s" % (user, status["full_text"]))
+def get_tweets(filename):
+    for line in open(filename):
+        yield twitter.Status.NewFromJsonDict(json.loads(line))
 
-#     #-----------------------------------------------------------------------
-#     # do a new query: who has RT'd this tweet?
-#     #-----------------------------------------------------------------------
-#     # retweets = twitter.statuses.retweets._id(_id = status["id"])
-#     # for retweet in retweets:
-#     #     print(" - retweeted by %s" % (retweet["user"]["screen_name"]))
-
-# def home():
-#     #-----------------------------------------------------------------------
-#     # request my home timeline
-#     # twitter API docs: https://dev.twitter.com/rest/reference/get/statuses/home_timeline
-#     #-----------------------------------------------------------------------
-#     statuses = twitter.statuses.home_timeline(count = 50)
-#     print(statuses)
-#     #-----------------------------------------------------------------------
-#     # loop through each of my statuses, and print its content
-#     #-----------------------------------------------------------------------
-#     for status in statuses:
-#         print("(%s) @%s %s" % (status["created_at"], status["user"]["screen_name"], status["text"]))
-
-# def search(text):
-#     #-----------------------------------------------------------------------
-#     # perform a user search 
-#     # twitter API docs: https://dev.twitter.com/rest/reference/get/users/search
-#     #-----------------------------------------------------------------------
-#     results = twitter.users.search(q = '"New Cross"')
-#     #-----------------------------------------------------------------------
-#     # loop through each of the users, and print their details
-#     #-----------------------------------------------------------------------
-#     for user in results:
-#         print("@%s (%s): %s" % (user["screen_name"], user["name"], user["location"]))
-# def tweet_search(text):
-#     #-----------------------------------------------------------------------
-#     # perform a basic search 
-#     # twitter API docs: https://dev.twitter.com/rest/reference/get/search/tweets
-#     #-----------------------------------------------------------------------
-#     terms = "pink elephants"
-#     created_at_format = '%a %b %d %H:%M:%S +0000 %Y'
-#     query = twitter.search.tweets(q = terms)
-#     results = query["statuses"]
-#     #-----------------------------------------------------------------------
-#     # take the timestamp of the first and last tweets in these results,
-#     # and calculate the average time between tweets.
-#     #-----------------------------------------------------------------------
-#     first_timestamp = datetime.strptime(results[0]["created_at"], created_at_format)
-#     last_timestamp = datetime.strptime(results[-1]["created_at"], created_at_format)
-#     total_dt = (first_timestamp - last_timestamp).total_seconds()
-#     mean_dt = total_dt / len(results)
-
-#     #-----------------------------------------------------------------------
-#     # print the average of the differences
-#     #-----------------------------------------------------------------------
-#     print("Average tweeting rate for '%s' between %s and %s: %.3fs" % (terms,
-#     results[-1]["created_at"], results[ 0]["created_at"], mean_dt))
-
-# # source from https://github.com/ideoforms/
+def get_replies(tweet):
+    user = tweet.user.screen_name
+    tweet_id = tweet.id
+    max_id = None
+    logging.info("looking for replies to: %s" % tweet_url(tweet))
+    while True:
+        q = urllib.parse.urlencode({"q": "to:%s" % user})
+        try:
+            replies = t.GetSearch(raw_query=q, since_id=tweet_id, max_id=max_id, count=100)
+        except twitter.error.TwitterError as e:
+            logging.error("caught twitter api error: %s", e)
+            time.sleep(60)
+            continue
+        for reply in replies:
+            logging.info("examining: %s" % tweet_url(reply))
+            if reply.in_reply_to_status_id == tweet_id:
+                logging.info("found reply: %s" % tweet_url(reply))
+                yield reply
+                # recursive magic to also get the replies to this reply
+                for reply_to_reply in get_replies(reply):
+                    yield reply_to_reply
+            max_id = reply.id
+        if len(replies) != 100:
+            break
