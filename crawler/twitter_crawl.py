1a2dcfacbbfb078ff979c86bc245e56987479485
hapidznur <hapidznur@gmail.com>
2018-10-31 09:35:16 +0700

    change stream output format

--- crawler/twitter_crawl.py
+++ crawler/twitter_crawl.py
@@ -1,6 +1,6 @@
 from decouple import config
 from datetime import datetime
-from model import mongo_add
+from model import MongoModel
 import twitter
 import logging
 import time
@@ -9,16 +9,30 @@ from urllib.parse import quote
 import json
 import datetime
 
+m = MongoModel()
+
 consumer_key = config('CONSUMER_KEY')
 consumer_secret = config('CONSUMER_SECRET')
 access_token = config('ACCESS_TOKEN')
 access_secret = config('ACCESS_SECRET')
+
+consumer_key_second = config('Consumer_API')
+consumer_secret_second = config('Consumer_API_Secret')
+access_token_second = config('Access_Token_Key')
+access_secret_second = config('Access_Token_Secret')
 
 api = twitter.Api(consumer_key=consumer_key,
     consumer_secret=consumer_secret,
     access_token_key=access_token,
     access_token_secret=access_secret,
     tweet_mode= 'extended',
+    sleep_on_rate_limit=True)
+
+auth = twitter.Api(consumer_key=consumer_key_second,
+    consumer_secret=consumer_secret_second,
+    access_token_key=access_token_second,
+    access_token_secret=access_secret_second,
+    tweet_mode='extended',
     sleep_on_rate_limit=True)
 
 def tweet_url(t):
@@ -71,8 +85,9 @@ def get_search(text):
             data['retweeted_username'] = result.retweeted_status.user.screen_name
             data['retweeted_id_tweet'] = result.retweeted_status.id_str
             data['tweet_created'] = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(result.retweeted_status.created_at,'%a %b %d %H:%M:%S +0000 %Y'))
+
             # get_replies(result)
-            mongo_add(data)
+            m.mongo_add(data, 'twitter')
             replies = {}
             for reply in get_replies(result.retweeted_status):
                 replies['id_tweet'] = reply.id_str
@@ -82,8 +97,8 @@ def get_search(text):
                 replies['user_id'] = reply.user.id
                 replies['in_reply_to_user_id'] = reply.in_reply_to_user_id
                 replies['in_reply_to_screen_name'] = reply.in_reply_to_screen_name
-                replies['from'] = 'search'
-            mongo_add(replies)
+                replies['type'] = 'replies'
+            m.mongo_add(replies, 'twitter')
 
 def stream():
     logging.basicConfig(filename="./logs/stream.log", level=logging.INFO)
@@ -91,12 +106,39 @@ def stream():
     logging.info("crawling on: %s" % datetime.datetime.now())
     output = './../data-twitter/%s.txt' % datetime.datetime.now().timestamp()
 
-    with open(output, 'a') as f:
-        # api.GetStreamFilter will return a generator that yields one status
-        # message (i.e., Tweet) at a time as a JSON dictionary.
-        for line in api.GetStreamFilter(track=text, filter_level='low'):
-            f.write(json.dumps(line))
-            f.write('\n')
+    # api.GetStreamFilter will return a generator that yields one status
+    # message (i.e., Tweet) at a time as a JSON dictionary.
+    
+    for line in auth.GetStreamFilter(track=text, filter_level='low'):
+        status = {}
+        str_parse = json.dumps(line)
+        tweet = twitter.Status.NewFromJsonDict(json.loads(str_parse))
+        status['id_str'] = tweet.id_str
+        if hasattr(tweet, 'full_text'):
+            status['text'] =  tweet.full_text               
+        else:
+            status['text'] =  tweet.text
+        
+        status['created_at'] = tweet.created_at
+        status['user_name'] = tweet.user.screen_name
+        status['user_id'] = tweet.user.id
+        status['retweet_count'] = tweet.retweet_count
+        status['favorite_count'] = tweet.favorite_count
+        logging.basicConfig(filename="./logs/stream.log", level=logging.INFO)
+        logging.info("crawling replies on: %s" % tweet.id)
+        m.mongo_add(status, 'twitter')
+        for reply in get_replies(tweet):
+            replies = {}
+            replies['created_at'] = reply.created_at
+            replies['id_tweet'] = reply.id_str
+            replies['user_name'] = reply.user.name
+            replies['user_username'] = reply.user.screen_name
+            replies['text'] = reply.full_text
+            replies['user_id'] = reply.user.id
+            replies['in_reply_to_status_id_str'] = reply.in_reply_to_user_id
+            replies['in_reply_to_screen_name'] = reply.in_reply_to_screen_name
+            replies['type'] = 'replies'
+            m.mongo_add(replies, 'twitter')
 
 def trends_location(woeid =  None):
     if woeid == None:
